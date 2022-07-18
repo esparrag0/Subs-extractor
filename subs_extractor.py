ac51feba32178153c85e0f887d31cfd35bb9a094
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-18 15:46:39 -0700

    Update subs_extractor.py

--- subs_extractor.py
+++ subs_extractor.py
@@ -1,11 +1,9 @@
-
-import sys, os
+import sys, subprocess
 from pathlib import Path
 
-def sub_extractor(folder_path):    
-    bat_file = folder_path / 'extractor.bat'
-    instructions = open(bat_file,'w+')
-    instructions.write('@echo off \n for /R %f IN (*.mkv) DO ffmpeg -i "%f" -map 0:s:0 "%~nf.ass" \n')
-
-
-    instructions.close
+folder_path = sys.argv[1]   
+bat_file = Path(folder_path) / 'extractor.bat'
+instructions = open(bat_file,'w+')
+instructions.write('@echo on \nfor /R %%f IN (*.mkv) DO ffmpeg -i "%%f" -map 0:s:0 "%%~nf.ass" \npause')
+instructions.close()
+subprocess.call(bat_file)
