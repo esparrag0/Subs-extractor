322d21615d435dd39e8a2ddff78cc77a26f7f40e
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-07-18 17:46:13 -0700

    Update subs_extractor.py

--- subs_extractor.py
+++ subs_extractor.py
@@ -5,6 +5,8 @@ folder_path = Path(sys.argv[1])
 os.chdir(folder_path)
 bat_file = folder_path / 'extractor.bat'
 instructions = open(bat_file,'w+')
-instructions.write('@echo on \nfor /R %%f IN (*.mkv) DO ffmpeg -i "%%f" -map 0:s:0 "%%~nf.ass" \npause')
+instructions.write('''@echo off
+for /R %%f IN (*.mkv) DO ffmpeg -i "%%f" -map 0:s:0 "%%~nf.ass" 
+(goto) 2>nul & del "%~f0"''')
 instructions.close()
 subprocess.call(bat_file)
