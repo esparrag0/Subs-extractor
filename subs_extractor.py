c635d2320d7ea564ed6314fdeea7b8dba4e4160b
HeWhoIsLearningToCode <esparrag0@protonmail.com>
2022-10-28 09:52:49 -0700

    Update subs_extractor.py

--- subs_extractor.py
+++ subs_extractor.py
@@ -8,5 +8,8 @@ instructions = open(bat_file,'w+')
 instructions.write('''@echo off
 for /R %%f IN (*.mkv) DO ffmpeg -i "%%f" -map 0:s:0 "%%~nf.ass" 
 (goto) 2>nul & del "%~f0"''')
+
+#Batch command for calling ffmpeg to extract the subs. The batch file will delete itself after running
+
 instructions.close()
 subprocess.call(bat_file)
